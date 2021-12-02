13111bb857e3650b572c76fbf5d40b7b30ca8ce3
Luis Carlin <luiscarlin@gmail.com>
2021-12-01 22:28:00 -0500

    join and part

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -1,11 +1,41 @@
 import express, { Application, Request, Response } from 'express';
+import initChatbot from './chatbot';
 
 const app: Application = express();
 
+app.use(express.json());
+
 const port: number = 3001;
 
-app.get('/toto', (req: Request, res: Response) => {
-  res.send('Hello toto');
+const chatbotClient = initChatbot();
+
+app.post('/join', async (req: Request, res: Response) => {
+  const { channel } = req.body;
+
+  const result = await chatbotClient.join(channel);
+
+  if (result.length > 0) {
+    res.json({ channel: result });
+  }
+  res.sendStatus(500);
+});
+
+app.get('/channels', (req: Request, res: Response) => {
+  res.json({ channels: chatbotClient.getChannels() });
+});
+
+app.post('/part', async (req: Request, res: Response) => {
+  const { channel } = req.body;
+
+  const result = await chatbotClient.part(channel);
+
+  console.log(result);
+
+  if (result.length > 0) {
+    res.json({ channel: result });
+  }
+
+  res.sendStatus(200);
 });
 
 app.listen(port, function () {
--- apps/server/src/chatbot.ts
+++ apps/server/src/chatbot.ts
@@ -1,53 +1,59 @@
 import tmi from 'tmi.js';
 
-// Define configuration options
-const opts = {
-  options: { debug: true },
-  identity: {
-    username: process.env.TWITCH_BOT_USERNAME,
-    password: process.env.TWITCH_BOT_OAUTH_TOKEN,
-  },
-  channels: [process.env.TWITCH_BOT_USERNAME],
-  connection: { reconnect: true, secure: true },
-} as tmi.Options;
+const initChatbot = () => {
+  // Define configuration options
+  const opts = {
+    options: { debug: true },
+    identity: {
+      username: process.env.TWITCH_BOT_USERNAME,
+      password: process.env.TWITCH_BOT_OAUTH_TOKEN,
+    },
+    channels: [process.env.TWITCH_BOT_USERNAME],
+    connection: { reconnect: true, secure: true },
+  } as tmi.Options;
 
-// Create a client with our options
-const client = new tmi.client(opts);
+  // Create a client with our options
+  const client = new tmi.client(opts);
 
-// Register our event handlers (defined below)
-client.on('message', onMessageHandler);
-client.on('connected', onConnectedHandler);
+  // Register our event handlers (defined below)
+  client.on('message', onMessageHandler);
+  client.on('connected', onConnectedHandler);
 
-// Connect to Twitch:
-client.connect();
+  // Connect to Twitch:
+  client.connect();
 
-// Called every time a message comes in
-function onMessageHandler(channel: string, userState: any, message: string, self: any) {
-  if (self || !message.startsWith('!')) {
-    return;
-  } // Ignore messages from the bot
+  return client;
 
-  console.log('debug', userState);
-  // Remove whitespace from chat message
-  const commandName = message.trim();
+  // Called every time a message comes in
+  function onMessageHandler(channel: string, userState: any, message: string, self: any) {
+    if (self || !message.startsWith('!')) {
+      return;
+    } // Ignore messages from the bot
 
-  // If the command is known, let's execute it
-  if (commandName === '!dice') {
-    const num = rollDice();
-    client.say(channel, `You rolled a ${num}`);
-    console.log(`* Executed ${commandName} command`);
-  } else {
-    console.log(`* Unknown command ${commandName}`);
+    console.log('debug', userState);
+    // Remove whitespace from chat message
+    const commandName = message.trim();
+
+    // If the command is known, let's execute it
+    if (commandName === '!dice') {
+      const num = rollDice();
+      client.say(channel, `You rolled a ${num}`);
+      console.log(`* Executed ${commandName} command`);
+    } else {
+      console.log(`* Unknown command ${commandName}`);
+    }
   }
-}
 
-// Function called when the "dice" command is issued
-function rollDice() {
-  const sides = 6;
-  return Math.floor(Math.random() * sides) + 1;
-}
+  // Function called when the "dice" command is issued
+  function rollDice() {
+    const sides = 6;
+    return Math.floor(Math.random() * sides) + 1;
+  }
 
-// Called every time the bot connects to Twitch chat
-function onConnectedHandler(addr: any, port: any) {
-  console.log(`* Connected to ${addr}:${port}`);
-}
+  // Called every time the bot connects to Twitch chat
+  function onConnectedHandler(addr: any, port: any) {
+    console.log(`* Connected to ${addr}:${port}`);
+  }
+};
+
+export default initChatbot;
